1bb5e09256385d48713290547383e0362d5f5171
Jooseulgi <iauuai21@gmail.com>
2023-02-02 23:39:23 +0900

    🚨 (eslint) : fix lint waring

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './store/index';
 
 const container = document.getElementById('root') as HTMLElement;
